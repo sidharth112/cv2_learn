a4619053316e0e4f8f1969d9ad95ea375a800916
sidharth112 <sidharth112@gmail.com>
2025-07-19 20:13:53 +0530

    Detect the 10 card in the image and transform it in Perspective projection

--- src/Chapter_5.cpp
+++ src/Chapter_5.cpp
@@ -4,6 +4,9 @@
 #include <opencv2/imgproc.hpp>  
 #include <print>  
 
+float w = 250.0f; // Width of the destination image
+float h = 350.0f; // Height of the destination image
+
 using namespace cv;
 using namespace std;
 
@@ -11,16 +14,27 @@ using namespace std;
 #if 1  
 int main()
 {
-	//Blank image  
-	Mat img(512, 512, CV_8UC3, Scalar(255, 255, 255)); // Corrected CV_8U3 to CV_8UC3
-	Rect roi(100, 100, 300, 300); // Define a region of interest (ROI)
+	// Load the image from file
+	std::string path = "resources/test.jpg";
+	Mat img = imread(path); // Load the image from the specified path
+	
+	Point2f srcPoints[4] = { {550,79},{856,148}, {752,532}, {444,462} };
+	Point2f dstPoints[4] = { {0,0}, {w,0}, {w,h}, {0,h} }; // Define the destination points for the perspective warp
 
-	putText(img, "Sidharth", Point(200, 200), FONT_HERSHEY_DUPLEX, 2, Scalar(0, 0, 0), 2); // Write text on the image
-	line(img, Point(130, 296), Point(382, 296), Scalar(255, 0, 0), 5); // Draw a blue diagonal line
-	rectangle(img, roi, Scalar(0, 69, 255), 2); // Draw a filled rectangle in the ROI
-	// Corrected the circle function call by adding the missing thickness parameter
-	//circle(img, Point(256, 256), 100, Scalar(0, 69, 255), FILLED);  
+	// Create a transformation matrix using the source and destination points
+	Mat matrix = getPerspectiveTransform(srcPoints, dstPoints);
+	// Apply the perspective warp to the image using the transformation matrix
+	Mat imgWarped;	
+	warpPerspective(img, imgWarped, matrix, Point(w,h));
+
+	for (int i = 0; i < 4; i++)
+	{
+		circle(img, srcPoints[i], 10, Scalar(0, 0, 255), FILLED); // Draw circles on the source points
+		//circle(imgWarped, dstPoints[i], 10, Scalar(0, 0, 255), FILLED); // Draw circles on the destination points
+	}
+	
 	imshow("Image", img);
+	imshow("Warped Image", imgWarped); // Display the warped image
 	waitKey(0); // Wait indefinitely until a key is pressed  
 }
 #endif  
