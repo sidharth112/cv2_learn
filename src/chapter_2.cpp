aae2a38e970a0e15e69cd6b4ff53e75c55a60ec5
sidharth112 <sidharth112@gmail.com>
2025-07-19 18:28:26 +0530

    Enable image functions

--- src/chapter_2.cpp
+++ src/chapter_2.cpp
@@ -13,7 +13,7 @@ int main()
 {
 	std::string imagePath = "resources/pic.jpg"; // Specify the path to your image file
 	Mat image = imread(imagePath, IMREAD_COLOR); // Read the image file
-	Mat ImageGrey, imgBlur, imgCanny;
+	Mat ImageGrey, imgBlur, imgCanny, imgDil, imgErode;
 	cvtColor(image, ImageGrey, COLOR_BGR2GRAY); // Convert the image to grayscale
 	GaussianBlur(image, imgBlur, Size(7, 7), 5, 0);
 	Canny(imgBlur, imgCanny, 50, 150); // Apply Canny edge detection
@@ -27,11 +27,18 @@ int main()
 	line(image, start, end, color, thickness);
 	imshow("Image Line", image); // Display the image with a line drawn on it
 
+
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3)); // Create a rectangular structuring element
+	dilate(imgCanny, imgDil, kernel, Point(-1, -1), 2); // Dilate the image
+	erode(imgDil, imgErode, kernel); // Erode the image
+	
 	imshow("Image Canny", imgCanny); // Display the Canny edge-detected image
 	imshow("Image", image); // Display the original image
 	imshow("Image Gray", ImageGrey);
 	// Display the grayscale image
 	imshow("Image Blur", imgBlur); // Display the blurred image
+	imshow("Image Dilation", imgDil); // Display the dilated image
+	imshow("Image Erode", imgErode); // Display the eroded image
 	waitKey(0); // Wait indefinitely until a key is pressed
 }
 #endif
